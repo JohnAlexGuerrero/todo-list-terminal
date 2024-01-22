5729204fb37e3c202e1251c107d50bcbc2450e20
johnalexguerrero <johnalexguerrero@gmail.com>
2024-01-21 23:18:56 -0500

    refactorizacion

--- app.py
+++ app.py
@@ -1,41 +1,103 @@
 from todo import Todo
-from ui import UI
 from database import database
-import re
+from datetime import datetime
 
 database.connect()
 # database.create_tables([Todo])
-todos = []
 
-def set_todos(list):
-    items_count = 0
-    
-    for item in list:
-        todo = {
-            "item": items_count + 1,
-            "id":item.id,
-            "name": item.name,
-            "completed":item.completed,
-            "create": item.createdAt
-        }
-        todos.append(todo)
+def getTodos():
+    return Todo.select()
 
-def main():
+def getTodo(todo):
+    todo = Todo.get(id=todo)
+    return f'{todo.id}->{'[X]' if todo.completed else '[ ]'} {todo.name.capitalize()}'
+
+def addTodo(todo):
+    todo = Todo.create(name=todo, completed=False, createdAt=datetime.today())
+
+
+def doneTodo(todo):
+    obj = Todo.get(id=todo)
+    obj.completed = True
+    obj.save()
+
+def updateTodo(todo_id,todo):
+    obj = Todo.get(todo_id)
+    obj.name = todo
+    obj.createdAt = datetime.today()
+    obj.save()
+
+def filterTodo():
+    pass
+
+def deleteTodo(todo):
+    qry=Todo.delete().where (Todo.id==todo)
+    qry.execute()
+
+def editTodo():
+    pass
+
+def show():
+    todos = getTodos()
+    for todo in todos:
+        print(getTodo(todo))
+
+def getFunctionIndex(name_function):
+    funct_tuple = (
+        'todos','todo','add','done','update','filter','delete','edit'
+    )
+    return funct_tuple.index(name_function)
+
+def getFunction(index, todo):
+    if index == 0:
+        show()
+    elif index == 1:
+        getTodo(todo=todo)
+    elif index == 2:
+        addTodo(todo=todo)
+    elif index == 3:
+        doneTodo(todo=todo)
+    elif index == 4:
+        updateTodo(todo=todo)
+    elif index == 5:
+        filterTodo()
+    elif index == 6:
+        deleteTodo(todo=todo),
+    elif index == 7:
+        editTodo()
     
-    ui = UI()
-    # opt = 'todos, todo 1, delete 2, done 2, create 2024-01-12, update 3, add'
-    
+    show()
+
+def main():   
+    todo = None
+ 
     while True:
-        actions = input('command>')
-        print('TodoList 2024')
+        command_input = input('command >>')
         
-        actions_str = [x for x in actions.split(' ')]
-
-        ui.actions(actions_str)
+        command_str = [x for x in command_input.split(' ')]
         
         
-        if actions == 'exit':
+        if command_input == 'exit':
             break
+        
+        print('TodoList 2024')
+        print('#  completed  task')
+        
+
+        if len(command_str) > 1:
+            result = map(lambda x : x, command_str[1:])
+            todo = " ".join(list(result))
+            
+        try:
+            index = getFunctionIndex(command_str[0])
+            getFunction(index, todo)
+            
+            todo = None
+
+        except:
+            print(f'Error command. {command_str[0]}')
+        
 
 if __name__ == '__main__':
     main()
+    print('GoodBye!')
